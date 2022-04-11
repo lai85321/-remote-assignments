34226ebe322d64cbabbf1b4c0e6232ea39e90d7b
tina.lei <lai85321@gmail.com>
2022-04-11 17:12:26 +0800

    change to arrow function

--- Week-4/Assignment-1/index.js
+++ Week-4/Assignment-1/index.js
@@ -1,10 +1,6 @@
 function delayedResult(n1, n2, delayTime, callback) {
     // your code here
-    const total = n1+n2
-    function sum(){
-        callback(total)
-    }
-    setTimeout(sum, delayTime)
+    setTimeout(()=>callback(n1+n2), delayTime)
 }
 
 delayedResult(4, 5, 3000, function(result) {
